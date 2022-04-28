a54c8f7f93b3fd4cdd39cbd5b0fa4b7d934cdcd9
Paul Thebaud <paul.thebaud29@gmail.com>
2022-04-28 22:50:23 +0200

    release: prepare release

--- src/ConsoleApplication.php
+++ src/ConsoleApplication.php
@@ -23,7 +23,7 @@ class ConsoleApplication extends Application
     /**
      * The current phpunitgen-console version.
      */
-    public const VERSION = '2.0.0';
+    public const VERSION = '3.0.0';
 
     /**
      * @var ContainerInterface
